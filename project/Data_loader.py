b4f91a962eab4b781b2b4d48f7e601b4e4d8ac05
Geurtjens, R.P. (Ruben) <r.geurtjens@student.ru.nl>
2021-05-22 13:46:12 +0200

    Finished U-net, and created train/val set

--- project/Data_loader.py
+++ project/Data_loader.py
@@ -4,6 +4,40 @@ from pycocotools import mask as cocomask
 import numpy as np
 import skimage.io as io
 import os
+from cv2 import resize
+import numpy as np 
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.dataloader import DataLoader
+
+class Data_loader():
+    
+    def __init__(self, annotation_path, image_path, val_size, batch_size, transform, shuffle = True, seed = 42):
+        self.annotation_path = annotation_path
+        self.image_path = image_path
+        self.val_size = val_size
+        self.batch_size = batch_size 
+        self.transform = transform
+        self.shuffle = shuffle
+        self.seed = seed
+     
+
+    def get_loaders(self):
+        data_set = ImageDataset(self.annotation_path, self.image_path)
+        data_size = len(data_set)
+        indices = list(range(data_size))
+        split = int(np.floor(self.val_size * data_size))
+        if self.shuffle:
+            np.random.seed(self.seed)
+            np.random.shuffle(indices)
+        train_indices, val_indices = indices[split:], indices[:split]
+
+        train_sampler = SubsetRandomSampler(train_indices)
+        val_sampler = SubsetRandomSampler(val_indices)
+        
+        train_loader = DataLoader(data_set, batch_size=self.batch_size, sampler=train_sampler)
+        val_loader = DataLoader(data_set, batch_size=self.batch_size, sampler=val_sampler)
+
+        return train_loader, val_loader
 
 class ImageDataset(Dataset):
     def __init__(self, annotaions_dir, images_dir, transform=None):
@@ -37,4 +71,11 @@ class ImageDataset(Dataset):
             mask = mask + m
         
         mask[mask > 0] = 1
+
+        if self.transform is not None:
+            augmentations = self.transform(image=I, mask=mask)
+            I = augmentations["image"]
+            mask = augmentations["mask"]
         return I, mask
+
+
--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from Data_loader import ImageDataset
+from Data_loader import ImageDataset, Data_loader
 from U_net import UNet
 import torch
 from torch.utils.data import DataLoader
@@ -6,21 +6,23 @@ import torch.optim as optim
 import torch.nn as nn
 import numpy as np
 
-TRAIN_IMAGES_DIRECTORY = "data/train/images"
+TRAIN_IMAGES_DIRECTORY = "small_data" # data/train/images"
 TRAIN_ANNOTATIONS_PATH = "data/train/annotation.json" #roughly 280000 images
 TRAIN_ANNOTATIONS_SMALL_PATH = "data/train/annotation-small.json" #8366 images
 
-BATCH_SIZE = 2
+BATCH_SIZE = 1
 EPOCHS = 10
 LEARNING_RATE = 1e-4 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def train(loader, model, optimizer, criterion):
+def train(train_loader, val_loader, model, optimizer, criterion):
     
+    best_score = 0
     for epoch in range(EPOCHS):
         avg_loss = []
-        for batch_idx, (data, mask) in enumerate(loader):
+        i = 0
+        for batch_idx, (data, mask) in enumerate(train_loader):
             data = data.to(DEVICE, dtype=torch.float)
             mask =  mask.float().unsqueeze(0).to(DEVICE)
 
@@ -30,17 +32,54 @@ def train(loader, model, optimizer, criterion):
             loss.backward()
 
             avg_loss.append(loss.item())
-            loss.step()
+            optimizer.step()
+            #if i % 5 == 0:
+            #    print(f"{i/len(loader)*100}%")
+            i+=1
+
+            dice_score = eval_model(val_loader, model)
+        if dice_score > best_score:
+            best_score = dice_score
+            print("saving model...")
+            save("model", model)
+            print("saved succesfully")
+
         print(f"epoch: {epoch}/{EPOCHS}     loss: {np.mean(avg_loss)}")
 
+def save(path, model):
+    torch.save(model.state_dict(), path)
+
+def eval_model(loader, model):
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(DEVICE, dtype=torch.float)
+            y = y.float().to(DEVICE).unsqueeze(1)
+            preds = torch.sigmoid(model(x))
+            preds = (preds > 0.5).float()
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += (2 * (preds * y).sum()) / (
+                (preds + y).sum() + 1e-8
+            )
+
+    print(f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}")
+    print(f"Dice score: {dice_score/len(loader)}")
+    model.train()
+    return dice_score
 
 if __name__ == "__main__":
+    
     model = UNet().to(DEVICE)
     
-    train_ds = ImageDataset(TRAIN_ANNOTATIONS_SMALL_PATH, TRAIN_IMAGES_DIRECTORY)
-    train_loader = DataLoader(train_ds, batch_size = BATCH_SIZE, shuffle=True)
+    dataset = Data_loader(TRAIN_ANNOTATIONS_SMALL_PATH, TRAIN_IMAGES_DIRECTORY, 0.2, BATCH_SIZE, transform=None, shuffle=True, seed=42 )
+    train_loader, val_loader = dataset.get_loaders()
 
     loss = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    train(train_loader, model, optimizer, loss)
+    train(train_loader, val_loader, model, optimizer, loss)
